574082dc36ce873e3f42fed5f1e752bb9c4a84bf
Kevin Rockwood <kevin@rockwood.me>
2017-06-24 16:47:18 +0900

    Add mix task for setting configuration

--- lib/exbee/frames/at_command_frame.ex
+++ lib/exbee/frames/at_command_frame.ex
@@ -5,7 +5,10 @@ defmodule Exbee.ATCommandFrame do
     def encode(%{id: id, command: command, value: nil}) do
       <<0x08, id, command::bitstring-size(16)>>
     end
-    def encode(%{id: id, command: command, value: value}) do
+    def encode(%{id: id, command: command, value: value}) when is_integer(value) do
+      <<0x08, id, command::bitstring-size(16), value>>
+    end
+    def encode(%{id: id, command: command, value: value}) when is_binary(value) do
       <<0x08, id, command::bitstring-size(16), value::binary>>
     end
   end
--- lib/mix/tasks/configuration.ex
+++ lib/mix/tasks/configuration.ex
@@ -22,6 +22,39 @@ defmodule Mix.Tasks.Exbee.Configuration do
     end
   end
 
+  defmodule Set do
+    use Mix.Task
+
+    alias Exbee.{Configuration, ATCommandFrame, Request}
+
+    @shortdoc "Set a configuration value. Options: [command=value --serial-port]"
+
+    @available_args [:serial_port]
+
+    def run(args) do
+      {options, command_pairs, _} = OptionParser.parse(args, switches: [serial_port: :string])
+
+      request_frames = Enum.map(command_pairs, &parse_command_pair/1)
+
+      {:ok, results} = Request.run(request_frames, options)
+
+      Enum.each results, fn({request_frame, response_frames}) ->
+        response = Enum.map_join(response_frames, ", ", &inspect(&1, base: :hex))
+        IO.puts("#{request_frame.command}: #{response}")
+      end
+    end
+
+    def parse_command_pair(command_pair) do
+      case String.split(command_pair, "=") do
+        [raw_command, raw_value] ->
+          {value, _} = Code.eval_string(raw_value)
+          %ATCommandFrame{command: String.upcase(raw_command), value: value}
+        _ ->
+          raise ArgumentError, message: "Unable to parse #{command_pair}."
+      end
+    end
+  end
+
   defmodule Options do
     use Mix.Task
 

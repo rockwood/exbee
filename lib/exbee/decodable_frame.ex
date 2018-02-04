fec56faba9222944e788b24486c206e3d3b698b9
Kevin Rockwood <kevin@rockwood.me>
2018-02-04 17:07:43 +0900

    Fix DecodeableFrame spec

--- lib/exbee/decodable_frame.ex
+++ lib/exbee/decodable_frame.ex
@@ -1,6 +1,6 @@
 defprotocol Exbee.DecodableFrame do
   @moduledoc "Protocol used to decode a binary message into a frame."
 
-  @spec decode(Exbee.DecodableFrame.t, binary) :: Exbee.DecodableFrame.t
+  @spec decode(Exbee.DecodableFrame.t(), binary) :: {:ok, Exbee.DecodableFrame.t()} | {:error, term}
   def decode(frame, encoded_frame)
 end
